6ecf7ea18c4140d513a17fdb86b35c8cfbb67086
username <nmkgchcb@gmail.com>
2022-04-26 22:07:29 +0900

    フレームとポイントの画像を認識可能に　精度難あり

--- main.py
+++ main.py
@@ -52,94 +52,73 @@ keras_mnist_model = load_model('./image_recognize/colab_mnist.hdf5')
 
 # prediction_array = keras_mnist_model.predict(test_data)
 
-result_array = []
+whole_result_array = []
 # files = glob.glob("./image_recognize/image/*")
-files = glob.glob("./image_recognize/frame_red_image/*")
-# files = ['./image_recognize/frame_red_image/WIN_20220329_10_43_11_Pro.jpg']
-data_nums = len(files)
+# files = glob.glob("./image_recognize/frame_red_image/*")
+files = ['./image_recognize/frame_point_image/WIN_20220426_20_46_57_Pro.jpg']
+image_nums = len(files)
+character_nums = 0
 print(files)
-for i in range(data_nums):
+for i in range(image_nums):
+    result_array = []
+
     print(files[i])
-    moji_img = cv2.imread(files[i])
-
-    # hsv_img = cv2.cvtColor(moji_img, cv2.COLOR_RGB2HSV)
-
-    # array_h = []
-    # array_s = []
-    # array_v = []
-
-    # sum = [0, 0, 0]
-    # for a in range(hsv_img.shape[0]):
-    #     for b in range(hsv_img.shape[1]):
-    #         sum[0] = sum[0] + hsv_img[a][b][0]
-    #         sum[1] = sum[1] + hsv_img[a][b][1]
-    #         sum[2] = sum[2] + hsv_img[a][b][2]
-    #         array_h.append(hsv_img[a][b][0])
-    #         array_s.append(hsv_img[a][b][1])
-    #         array_v.append(hsv_img[a][b][2])
-    
-    # average_hsv = [0, 0, 0]
-
-    # average_hsv[0] = sum[0] / (hsv_img.shape[0] * hsv_img.shape[1])
-    # average_hsv[1] = sum[1] / (hsv_img.shape[0] * hsv_img.shape[1])
-    # average_hsv[2] = sum[2] / (hsv_img.shape[0] * hsv_img.shape[1])
-
-    # std_d_h = np.std(array_h)
-    # std_d_s = np.std(array_s)
-    # std_d_v = np.std(array_v)
-
-    # print("average_h:", average_hsv[0])
-    # print("average_s:", average_hsv[1])
-    # print("average_v:", average_hsv[2])
-
-    # print("std_d_h:", std_d_h)
-    # print("std_d_s:", std_d_s)
-    # print("std_d_v:", std_d_v)
-
-    extract_img = extract_frame(moji_img, files[i], None)
-
-    # plt.imshow(moji_img)
-    # plt.gray()
-    # plt.show()
+    img = cv2.imread(files[i])
 
     WIDTH = 1280
-    h, w = extract_img.shape[:2]
+    h, w = img.shape[:2]
     HEIGHT = round(h * (WIDTH / w))
-    extract_img = cv2.resize(extract_img, dsize=(WIDTH, HEIGHT))
-    input_img = transform_inputImg(extract_img, files[i], IMG_ROWS, IMG_COLS)
-    # print(input_img)
+    img = cv2.resize(img, dsize=(WIDTH, HEIGHT))
 
-    handwritten_number_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    character_img_array, frame_number = extract_frame(img, None)
+    
+    for index in frame_number:
+        character_nums += 1
+        character_img = character_img_array[index]
 
-    prediction_result = keras_mnist_model.predict(input_img)
+        # plt.imshow(moji_img)
+        # plt.gray()
+        # plt.show()
 
-    print(prediction_result[0])
+        input_img = transform_inputImg(character_img, files[i], IMG_ROWS, IMG_COLS)
+        # print(input_img)
 
-    prediction_number = np.argmax(prediction_result[0])
+        handwritten_number_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-    result_array.append(handwritten_number_names[prediction_number])
+        prediction_result = keras_mnist_model.predict(input_img)
 
-print(result_array)
+        print(prediction_result[0])
+
+        prediction_number = np.argmax(prediction_result[0])
+
+        result_array.append(handwritten_number_names[prediction_number])
+
+    whole_result_array.append(result_array)
+
+print(whole_result_array)
 
 # answer  = ['5', '3', '1', '1', '5', '3', '2', '4', '6', '8', 
 #            '6', '1', '2', '2', '8', '0', '1', '2', '3', '4', 
 #            '5', '6', '7', '8', '9', '1', '2', '3', '4', '5', 
 #            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
 #            '2', '9', '4', '8', '5', '5', '3', '9', '6', '3']           
-answer = ['3', '4', '9', '2', '4']
+# answer = ['3', '4', '9', '2', '4']
+answer = [['3', '9', '4', '2', '1']]
 
 
 cnt = 0
-for i in range(data_nums):
-    if(result_array[i] == answer[i]):
-        cnt += 1
+for i in range(image_nums):
+    for j in range(len(whole_result_array[i])):
+        if(whole_result_array[i][j] == answer[i][j]):
+            cnt += 1
 
 correct_result_array = [[0] * NUM_CLASSES for i in range(NUM_CLASSES)]
-for i in range(data_nums):
-    correct_result_array[int(answer[i])][int(result_array[i])] += 1
+for i in range(image_nums):
+    for j in range(len(whole_result_array[i])):
+        correct_result_array[int(answer[i][j])][int(whole_result_array[i][j])] += 1
 
 for i in range(NUM_CLASSES):
     print(str(i) + ":", end='')
     print(correct_result_array[i])
 
-print("精度:", cnt / data_nums)
+print("精度:", cnt / character_nums)
